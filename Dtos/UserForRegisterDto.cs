0f20ba8002f9df79544177fe0a8694a5ef63e700
navingenex <navingenex@gmail.com>
2018-06-26 20:34:57 +0530

    register, login with token initialized

---
+++ Dtos/UserForRegisterDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(8,MinimumLength=4,ErrorMessage="You must specify a password between 4 to 8 charecter")]
+        public string Password { get; set; }
+    }
+}
